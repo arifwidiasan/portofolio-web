455fa533e1fbbd1cc414245a588273e7fa89895f
arifwidiasan <arifw.subagio17@gmail.com>
2024-02-17 19:29:17 +0700

    add navigation bar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 
@@ -33,7 +33,7 @@ const HeroSection = () => {
                 Quisquam, quos.
             </p>
             <div>
-                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Hire Me!</button>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white font-semibold">Hire Me!</button>
                 <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"><span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span></button>
             </div>
         </div>
