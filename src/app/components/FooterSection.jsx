d83b9fd1ebef1ea7e7cdcd2b04a853700d6a0d2f
arifwidiasan <arifw.subagio17@gmail.com>
2024-02-21 00:23:19 +0700

    add portofolio

--- src/app/components/FooterSection.jsx
+++ src/app/components/FooterSection.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const FooterSection = () => {
     return (
         <footer className="footer border z-10 border-t-[#4b5584] border-l-transparent border-r-transparent text-white">
           <div className="container p-12 flex justify-between">
-            <span>LOGO</span>
+            <Link href={"#"} className="hidden md:block">
+                <Image 
+                    src="/image/logo-transparent.png" 
+                    alt="Logo Image"    
+                    width={150} height={30} />
+            </Link>
             <p className="text-slate-600">All rights reserved.</p>
           </div>
         </footer>
